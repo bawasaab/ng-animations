aaa2107bfebb430bf0a7b4f1e2660b2117e6d73e
Deepak Bawa <bawa_d@ymail.com>
2022-07-16 22:23:19 +0530

    intro triggers and states

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('clickedState', [
+
+      state(
+        'default', 
+        style({
+          backgroundColor: 'orange',
+          width: '100px',
+          height: '100px',
+        })
+      ),
+
+      state(
+        'clicked', 
+        style({
+          backgroundColor: 'blue',
+          width: '300px',
+          height: '50px',
+        })
+      ),
+
+      transition(
+        'default => clicked', 
+        animate('1s 500ms ease-in')
+      )
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'ng-animations';
+  clickInfo = 'default';
+
+  onClickSimple() {
+    this.clickInfo = 'clicked';
+    setTimeout(() => {
+      this.clickInfo = 'default';
+    }, 3000)
+  }
 }
